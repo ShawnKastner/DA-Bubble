d8f1bf2e40ec6bc73693ca898d8192012706cd5f
ShawnKastner <shawn.kastner@icloud.com>
2023-09-15 12:29:59 +0200

    add own profile at the top with HTML and SCSS, function to show the profile avatar and the displayName

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EditMemberComponent } from './edit-member/edit-member.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   profileDetails: any;
+  userData: any;
+  userID!: string;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private firestore: AngularFirestore
+  ) {
+    const userDataString = localStorage.getItem('user');
+    if (userDataString) {
+      this.userData = JSON.parse(userDataString);
+      this.userID = this.userData.uid;
+    }
+  }
+
+  ngOnInit() {
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.firestore
+      .collection('users')
+      .doc(this.userID)
+      .valueChanges()
+      .subscribe((data: any) => {
+        this.profileDetails = data;
+      });
+  }
 
   openEditMemberDialog() {
     this.dialog.open(EditMemberComponent, {
